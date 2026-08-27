1cd923b596ab5822ebfbd90b08175fd50be99ee1
SeemaBachwal <37027577+SeemaBachwal@users.noreply.github.com>
2026-08-26 19:14:22 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int left = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int right = 0; right < nums.length; right++) {
+            if (right - left > k) {
+                set.remove(nums[left]);
+                left++;
+            }
+
+            if (!set.add(nums[right])) {
+                return true;
+            }
+
+            //set.add(nums[right]);
+        }
+
+        return false;
+    }
+}
