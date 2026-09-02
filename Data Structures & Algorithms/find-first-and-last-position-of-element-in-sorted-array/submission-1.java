a1cfb4e9baa4278ef47c61d529f16469bf59e113
Seema Bachwal <37027577+SeemaBachwal@users.noreply.github.com>
2026-09-01 19:36:01 -0700

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-1.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int firstIdx = findIndex(nums, target, true);
+        int lastIdx = findIndex(nums, target, false);
+
+        return new int[] {firstIdx, lastIdx};
+    }
+
+    public int findIndex(int[] nums, int target, boolean leftIdx) {
+        int low = 0, high = nums.length - 1;
+
+        int idx = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (target > nums[mid]) {
+                low = mid + 1;
+
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            }
+
+            else {
+                idx = mid;
+
+                if (leftIdx) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+
+        return idx;
+    }
+}
