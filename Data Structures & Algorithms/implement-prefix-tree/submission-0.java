93504f6c86151c15538f8d1f9eb73fca2c569ebe
Seema Bachwal <37027577+SeemaBachwal@users.noreply.github.com>
2026-09-23 22:49:01 -0700

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,54 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndingWord = false;
+}
+
+class PrefixTree {
+    TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+
+            curr = curr.children[c - 'a'];
+        }
+
+        curr.isEndingWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                return false;
+            }
+
+            curr = curr.children[c - 'a'];
+        }
+
+        return curr.isEndingWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+
+        for (char c : prefix.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                return false;
+            }
+
+            curr = curr.children[c - 'a'];
+        }
+
+        return true;
+    }
+}
